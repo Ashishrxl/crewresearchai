ec3011483ec32e4b834c3f5b769655d177cdef1a
Ashish Jaiswal <67005570+Ashishrxl@users.noreply.github.com>
2026-07-26 17:35:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -147,12 +147,12 @@ with st.expander("🛠️ Tool & Model Selection Configuration"):
     st.subheader("🧠 Multi-Model Assignment")
     planner_writer_model = st.selectbox(
         "Planner & Writer Model",
-        ["gemini/gemini-2.5-pro", "gemini/gemini-2.5-flash", "gemini/gemini-3.5-flash", "gemini/gemini-3.5-flash-lite", gemini-3.1-flash-lite"],
+        ["gemini/gemini-2.5-pro", "gemini/gemini-2.5-flash", "gemini/gemini-3.5-flash", "gemini/gemini-3.5-flash-lite", "gemini/gemini-3.1-flash-lite"],
         index=1
     )
     research_checker_model = st.selectbox(
         "Researcher & Checker Model",
-        ["gemini/gemini-2.5-flash", "gemini/gemini-3.5-flash", "gemini/gemini-3.5-flash-lite", gemini-3.1-flash-lite"],
+        ["gemini/gemini-2.5-flash", "gemini/gemini-3.5-flash", "gemini/gemini-3.5-flash-lite", "gemini/gemini-3.1-flash-lite"],
         index=0
     )
 
